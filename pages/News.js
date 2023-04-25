c3b8a19bfff432ea98478d574f10f2f39ccc009f
Vs-karma <81770024+Vs-karma@users.noreply.github.com>
2023-04-25 22:43:51 +0530

    change

--- pages/News.js
+++ pages/News.js
@@ -6,7 +6,7 @@ function News(props) {
     const [page, setPage] = useState(1);
     const [totalResults, setTotalResults] = useState(0);
     const updateNow = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&country=general&apiKey=98421eb58c7c4258bf674a2a932c747f&page=${page}&pagesize=10`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&country=general&apiKey=7e9e1305ea184d998fdf3e2014c7910a&page=${page}&pagesize=10`;
         // let data = await fetch(url)
         const data = await (await fetch(url)).json();
         setArticles(data.articles);
@@ -29,33 +29,21 @@ function News(props) {
     
       }
     
-    
-      const fetchMoreData = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&country=${props.category}&apiKey=98421eb58c7c4258bf674a2a932c747f&page=${page + 1}&pagesize=${props.pageSize}`;
-        setPage(page + 1);
-        let data = await fetch(url);
-        let parseDat = await data.json();
-        console.log(parseDat);
-        setArticles(parseDat.articles);
-        setTotalResults(parseDat.totalResults);
-        setLoading(false);
-    
-      };
 
     return (
         <div>
-            <div className="container my-3 ">
-                <div className="row">
+            <div className="container_">
+                <div className="row_">
                     {articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
+                        return <div className="col_" key={element.url}>
                             <NewsItem title={element.title ? element.title : ""} discription={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://image.cnbcfm.com/api/v1/image/107004840-1642794265011-2022-01-21T190502Z_3_LYNXMPEI0K10G_RTROPTP_4_SHELL-OUTLOOK.JPG?v=1642794359"} newsUrl={element.url} source={element.source.name} />
                         </div>
                     })}
                 </div>
             </div>
-            <div className="container d-flex justify-content-between">
-                <button disabled={page <= 1} type="button" onClick={handlePrevClick} className="btn btn-primary btn-lg"> &larr; Prev</button>
-                <button disabled={page + 1 > Math.ceil(totalResults / 18)} type="button" onClick={handleNextClick} className="btn btn-secondary btn-lg">Next &rarr;</button>
+            <div className="ldBtn">
+                <button disabled={page <= 1} type="button" onClick={handlePrevClick} className="bttn" style={{backgroundColor:"grey"}}> &larr; Prev</button>
+                <button disabled={page + 1 > Math.ceil(totalResults / 18)} type="button" onClick={handleNextClick} className="bttn" style={{backgroundColor:"blue"}}>Next &rarr;</button>
             </div>
         </div>
     )
--- pages/NewsItem.js
+++ pages/NewsItem.js
@@ -2,23 +2,40 @@ import React from 'react'
 
 function NewsItem({ title, discription, imageUrl, newsUrl, source }) {
     return (
-        <div className='my-3'>
-            <div className="card">
-                <div style={{ display: "flex", justifyContent: "flex-end", position: "absolute", right: "0" }}>
-                    <span className=" badge rounded-pill bg-danger" >
-                        {source}
-                    </span>
+        <>
+            {/* <div className='my-3'>
+                <div className="card">
+                    <div style={{ display: "flex", justifyContent: "flex-end", position: "absolute", right: "0" }}>
+                        <span className=" badge rounded-pill bg-danger" >
+                            {source}
+                        </span>
+                    </div>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title" style={{ color: "black" }}>{title}...</h5>
+                        <p className="card-text" style={{ color: "black" }}>{discription}...</p>
+                        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
                 </div>
-                <img src={imageUrl} className="card-img-top" alt="..." />
-                <div className="card-body">
-                    <h5 className="card-title" style={{color:"black"}}>{title}...</h5>
-                    <p className="card-text" style={{color:"black"}}>{discription}...</p>
-                    {/* <a href="#" rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a> */}
-                    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-primary">Read More</a>
-                </div>
-            </div>
 
-        </div>
+            </div> */}
+            <div className="newItem">
+                <div className="itemLeft">
+                    <div className="itemSource">
+                        {source}
+                    </div>
+                    <img className='Image' src={imageUrl} alt="..." />
+                </div>
+                <div className="itemRight">
+                    <div className="itemBody">
+                        <p className="itemHeading">{title}...</p>
+                        <p className="itemDisc">{discription}</p>
+                        <a href={newsUrl} target='_blank' className="button">Read More</a>
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
 
